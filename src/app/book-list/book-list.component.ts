462d27bbe00055e76c9c9ac2c5b661fcdaea6bea
bluemyria <bluemyria@users.noreply.github.com>
2019-09-24 14:47:37 +0200

    iter-3-http

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'lb-book-list',
@@ -8,12 +9,12 @@ import { BookStoreService } from '../shared/book-store.service';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-  books: Book[];
+  books: Book[] = [];
 
   constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
-    this.books = this.bs.getAll();
+    this.bs.getAll().subscribe( res => this.books = res);
   }
 
 }
--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,49 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookStoreService {
 
-  books: Book[];
+  private api = 'https://api3.angular-buch.com';
 
-  constructor() {
-    this.books = [
-      {
-        isbn: '9783864906466',
-        title: 'Angular',
-        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
-        published: new Date(2019, 4, 30),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices - mit NativeScript und NgRx',
-        rating: 5,
-        thumbnails: [{
-          url: 'https://ng-buch.de/buch1.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Die Autoren führen Sie mit einem anspruchsvollen Beispielprojekt durch die Welt von Angular...'
-      },
-      {
-        isbn: '9783864903274',
-        title: 'React',
-        authors: ['Oliver Zeigermann', 'Nils Hartmann'],
-        published: new Date(2016, 6, 17),
-        subtitle: 'Die praktische Einführung in React, React Router und Redux',
-        rating: 3,
-        thumbnails: [{
-          url: 'https://ng-buch.de/buch2.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'React ist ein JavaScript-Framework zur Entwicklung von Benutzeroberflächen...'
-      }
-    ];
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Book[]> {
+    return this.http.get<any>(`${this.api}/books`);
   }
 
-  getAll(): Book[] {
-    return this.books;
+  getSingle(isbn: string): Observable<Book> {
+    return this.http.get<any>(`${this.api}/book/${isbn}`);
   }
 
-  getSingle(isbn: string): Book {
-    return this.books.find( book => book.isbn === isbn);
+  remove(isbn: string): Observable<any> {
+    return this.http.delete(
+      `${this.api}/book/${isbn}`,
+      {responseType: 'text'}
+    );
   }
+
 }
